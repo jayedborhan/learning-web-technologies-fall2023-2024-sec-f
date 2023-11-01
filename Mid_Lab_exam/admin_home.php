8d3290f4dc30879984d1b530eaf2d064076bb0c8
MD Borhan Uddin <102826817+jayedborhan@users.noreply.github.com>
2023-11-01 16:10:11 +0600

    Mid_lab_exam

---
+++ Mid_Lab_exam/admin_home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome BOB!</title>
+</head>
+<body>
+    <h1 style="font-size: 36px; text-align: center;">Welcome BOB!</h1>
+</body>
+
+
+</html>
+<center align
+<li> <a href="profile.php">Profile</a></li>
+ <li> <a href="changepassword.php">Change Password</a></li>
+ <li> <a href="Viewusers.php">View profile</a></li>
+ <li> <a href="Logout.php">Logout</a></li>
